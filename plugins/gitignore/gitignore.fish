9edf5ca5bd567186e389affc519b8f1d85d5e351
mattmc3 <mattmc3@gmail.com>
2024-09-02 23:13:42 -0400

    Add gitignore plugin

---
+++ plugins/gitignore/gitignore.fish
@@ -0,0 +1,30 @@
+function gi --description "Generate a git ignore file"
+    set --local usage "usage: gi [-h|--help] [-l|--list] <TEMPLATES...>"
+
+    argparse --name=gi 'h/help' 'l/list' -- $argv
+    or begin
+        echo $usage
+        return 1
+    end
+
+    if test -n "$_flag_list"
+        set --local thisdir (path resolve (status dirname))
+        if test (find $thisdir -type f -name ".gi_templates" -mmin +1200 | count) -eq 0
+            curl -sfL https://www.toptal.com/developers/gitignore/api/list | tr "," "\n" > $thisdir/.gi_templates
+        end
+        cat $thisdir/.gi_templates
+        return
+    end
+
+    if test (count $argv) -eq 0 || test -n "$_flag_help"
+        echo $usage
+        return
+    end
+
+    set --local templates (string join , $argv)
+    curl -fLw '\n' https://www.toptal.com/developers/gitignore/api/$templates
+end
+
+complete --command gi --no-files -a "(gi --list)"
+complete --command gi --no-files -s h -l help -d "Show usage"
+complete --command gi --no-files -s l -l list -d "List templates"
