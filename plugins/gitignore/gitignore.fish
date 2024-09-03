f80169eb49b9bfc0beb22a649c72e75d962e4b78
mattmc3 <mattmc3@gmail.com>
2024-09-02 23:30:10 -0400

    make pretty

--- plugins/gitignore/gitignore.fish
+++ plugins/gitignore/gitignore.fish
@@ -1,7 +1,7 @@
 function gi --description "Generate a git ignore file"
     set --local usage "usage: gi [-h|--help] [-l|--list] <TEMPLATES...>"
 
-    argparse --name=gi 'h/help' 'l/list' -- $argv
+    argparse --name=gi h/help l/list -- $argv
     or begin
         echo $usage
         return 1
@@ -10,7 +10,7 @@ function gi --description "Generate a git ignore file"
     if test -n "$_flag_list"
         set --local thisdir (path resolve (status dirname))
         if test (find $thisdir -type f -name ".gi_templates" -mmin +1200 | count) -eq 0
-            curl -sfL https://www.toptal.com/developers/gitignore/api/list | tr "," "\n" > $thisdir/.gi_templates
+            curl -sfL https://www.toptal.com/developers/gitignore/api/list | tr "," "\n" >$thisdir/.gi_templates
         end
         cat $thisdir/.gi_templates
         return
