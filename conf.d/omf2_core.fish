66eaffbb46ce9ff74d61f05c8148f1d22baa9c82
mattmc3 <mattmc3@gmail.com>
2024-09-09 14:06:35 -0400

    Remove infinite loop

--- conf.d/omf2_core.fish
+++ conf.d/omf2_core.fish
@@ -1,7 +1,3 @@
-if not type -q omf2 && type -q fisher
-    fisher install oh-my-fish-2/omf2
-end
-
 # Support Fisher's plugin events
 function _omf2_core_install --on-event omf2_core_install
     echo omf2 handle install oh-my-fish-2/core
