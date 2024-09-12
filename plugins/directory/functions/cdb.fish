78c023b61583741b004b58266152bbcbc79d886a
mattmc3 <mattmc3@gmail.com>
2024-09-12 10:20:43 -0400

    Add comment

--- plugins/directory/functions/cdb.fish
+++ plugins/directory/functions/cdb.fish
@@ -2,6 +2,9 @@
 # Copyright (c) 2016 Diego Zamboni
 # Home: https://github.com/zzamboni/plugin-cdb
 
+# Usage:
+# some-command /path/to/some/file
+# cdb <ALT+up>
 function cdb -d "cd to base directory of argument"
 	cd (dirname $argv[1])
 end
