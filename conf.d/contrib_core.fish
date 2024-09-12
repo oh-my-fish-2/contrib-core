11887a8d3f602b566e8a62990ec03b5625d361a6
mattmc3 <mattmc3@gmail.com>
2024-09-12 14:32:11 -0400

    Update conf.d logic

--- conf.d/contrib_core.fish
+++ conf.d/contrib_core.fish
@@ -1,15 +1,20 @@
 # Support Fisher's plugin events
-function _omf2_core_install --on-event omf2_core_install
-    if not type -q omf2 && type -q fisher
+if not type -q omf2
+    function omf2
+        functions -e omf2
         fisher install oh-my-fish-2/omf2
+        omf2 $argv
     end
+end
+
+function _contrib_core_install --on-event contrib_core_install
     omf2 fisher-event install oh-my-fish-2/contrib-core
 end
 
-function _omf2_core_update --on-event omf2_core_update
+function _contrib_core_update --on-event contrib_core_update
     omf2 fisher-event update oh-my-fish-2/contrib-core
 end
 
-function _omf2_core_uninstall --on-event omf2_core_uninstall
+function _contrib_core_uninstall --on-event contrib_core_uninstall
     omf2 fisher-event uninstall oh-my-fish-2/contrib-core
 end
