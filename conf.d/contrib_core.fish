33885ac3f7ab1c391018cfe55d24e7b0a948d3bf
mattmc3 <mattmc3@gmail.com>
2024-09-12 15:18:25 -0400

    Use new omf2 api

--- conf.d/contrib_core.fish
+++ conf.d/contrib_core.fish
@@ -1,20 +1,21 @@
 # Support Fisher's plugin events
-if not type -q omf2
-    function omf2
+
+function _omf2
+    if not type -q omf2
         functions -e omf2
         fisher install oh-my-fish-2/omf2
-        omf2 $argv
     end
+    omf2 $argv
 end
 
 function _contrib_core_install --on-event contrib_core_install
-    omf2 fisher-event install oh-my-fish-2/contrib-core
+    _omf2 install oh-my-fish-2/contrib-core
 end
 
 function _contrib_core_update --on-event contrib_core_update
-    omf2 fisher-event update oh-my-fish-2/contrib-core
+    _omf2 update oh-my-fish-2/contrib-core
 end
 
 function _contrib_core_uninstall --on-event contrib_core_uninstall
-    omf2 fisher-event uninstall oh-my-fish-2/contrib-core
+    _omf2 uninstall oh-my-fish-2/contrib-core
 end
