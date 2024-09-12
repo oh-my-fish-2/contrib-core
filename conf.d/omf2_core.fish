97598021ba40a322fe2f92313a544a9523e57a00
mattmc3 <mattmc3@gmail.com>
2024-09-12 10:38:23 -0400

    Update core events

--- conf.d/omf2_core.fish
+++ conf.d/omf2_core.fish
@@ -1,12 +1,16 @@
+if not type -q omf2 && type -q fisher
+    fisher install oh-my-fish-2/omf2
+end
+
 # Support Fisher's plugin events
 function _omf2_core_install --on-event omf2_core_install
-    echo omf2 handle install oh-my-fish-2/core
+    omf2 fisher-event install oh-my-fish-2/core
 end
 
 function _omf2_core_update --on-event omf2_core_update
-    echo omf2 handle update oh-my-fish-2/core
+    omf2 fisher-event update oh-my-fish-2/core
 end
 
 function _omf2_core_uninstall --on-event omf2_core_uninstall
-    echo omf2 handle uninstall oh-my-fish-2/core
+    omf2 fisher-event uninstall oh-my-fish-2/core
 end
