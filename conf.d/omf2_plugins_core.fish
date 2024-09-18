8d7c72ae1c85f2dedcc247f13f5f5193352b714c
mattmc3 <mattmc3@gmail.com>
2024-09-18 08:11:59 -0400

    Update git url

--- conf.d/omf2_plugins_core.fish
+++ conf.d/omf2_plugins_core.fish
@@ -3,19 +3,19 @@
 function _omf2
     if not type -q omf2
         functions -e omf2
-        fisher install oh-my-fish-2/omf2
+        fisher install omf2/omf2
     end
     omf2 $argv
 end
 
 function _omf2_plugins_core_install --on-event omf2_plugins_core_install
-    _omf2 fisher-event install oh-my-fish-2/plugins-core
+    _omf2 fisher-event install omf2/plugins-core
 end
 
 function _omf2_plugins_core_update --on-event omf2_plugins_core_update
-    _omf2 fisher-event update oh-my-fish-2/plugins-core
+    _omf2 fisher-event update omf2/plugins-core
 end
 
 function _omf2_plugins_core_uninstall --on-event omf2_plugins_core_uninstall
-    _omf2 fisher-event uninstall oh-my-fish-2/plugins-core
+    _omf2 fisher-event uninstall omf2/plugins-core
 end
